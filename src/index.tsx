bf6f230377ee9f98bed27a60945553a8fd02abd5
YuriRusakovich <1970.gippo@gmail.com>
2022-06-02 11:35:41 +0300

    converted todo list to PWA

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from '@components/app';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 render(<App />, document.getElementById("root"));
+
+serviceWorkerRegistration.register();
